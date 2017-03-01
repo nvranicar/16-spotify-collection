18d232d0067233338c71d538232e7a9b9703f7be
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-28 19:29:41 -0600

    got the view to update, only stuck on pictures and links

---
+++ app/song-item.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="song-item">
+    <div class="track">
+      <div class="panel-block media">
+        <div class="media-left">
+          <img src="{{songs.images[0].url}}" alt="" class="track__media track__frame">
+        </div>
+        <div class="media-content">
+          <h4 class="track__artist">{{songs.artists[0].name}}</h4>
+          <p class="track__name">{{songs.name}}</p>
+        </div>
+        <div class="media-right">
+          <a href="{{songs.href}}" class="fa fa-spotify track__frame"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['songs'],
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
